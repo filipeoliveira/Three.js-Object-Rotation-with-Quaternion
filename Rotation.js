590dce805e2d7bfe44bc58d9bd2d3cddc13f8da3
filipeoliveira <filipe.jtoliveira@gmail.com>
2017-05-04 02:35:56 -0300

    Initial commit of "Trabalho 01 - CG"
    Cube and Tetrahedron with arcball rotation working.

--- Rotation.js
+++ Rotation.js
@@ -1,10 +1,19 @@
-window.log = function()
-{
-	if (this.console)
-	{
-		console.log(Array.prototype.slice.call(arguments));
-	}
-};
+/*
+#TODO
+
+1. Fazer seleção dos poligonos via divs.
+- Limpar os poligonos existentes na cena
+- Carregar o poligono adequado.
+- Adicionar funcionalidade do RESET para carregar o poligono atual novamente.
+
+2. Adicionar cor ao poligono tetrahedron.
+
+3. Ao apertar right-click fixar a face no meio.
+
+4.Realizar uma busca em profundidade para determinar o núero dos vértices a serem abertos.
+- Abrir o poligono cubo (inicialmente);  (??)
+
+*/
 
 // Namespace
 var Defmech = Defmech ||
@@ -18,7 +27,8 @@ Defmech.RotationWithQuaternion = (function()
 
 	var camera, scene, renderer;
 
-	var cube, plane;
+	// var selectedPolygon = 'cube';
+	var selectedPolygon = 'tetrahedron';
 
 	var mouseDown = false;
 	var rotateStartPoint = new THREE.Vector3(0, 0, 1);
@@ -52,53 +62,105 @@ Defmech.RotationWithQuaternion = (function()
 		info.innerHTML = 'Drag to spin the cube';
 		container.appendChild(info);
 
+		var reset = document.createElement('div');
+		reset.style.position = 'absolute';
+		reset.style.top = '10px';
+		reset.style.right = '20px';
+		reset.style.width = '8em';
+		reset.style.textAlign = 'center';
+		reset.innerHTML = 'RESET';
+		reset.style.backgroundColor = 'lightcoral';
+		reset.style.cursor = 'pointer';
+		reset.style.color = 'white';
+		reset.style.padding = '1em';
+		container.appendChild(reset);
+
+		var tetrahedron_btn = document.createElement('div');
+		tetrahedron_btn.style.position = 'absolute';
+		tetrahedron_btn.style.top = '70px';
+		tetrahedron_btn.style.right = '20px';
+		tetrahedron_btn.style.width = '8em';
+		tetrahedron_btn.style.textAlign = 'center';
+		tetrahedron_btn.innerHTML = 'TETRAHEDRON';
+		tetrahedron_btn.style.backgroundColor = 'lightgreen';
+		tetrahedron_btn.style.cursor = 'pointer';
+		tetrahedron_btn.style.color = 'white';
+		tetrahedron_btn.style.padding = '1em';
+		container.appendChild(tetrahedron_btn);
+
+		document.get
+
+		var octahedron_btn = document.createElement('div');
+		octahedron_btn.style.position = 'absolute';
+		octahedron_btn.style.top = '130px';
+		octahedron_btn.style.right = '20px';
+		octahedron_btn.style.width = '8em';
+		octahedron_btn.style.textAlign = 'center';
+		octahedron_btn.innerHTML = 'OCTAHEDRON';
+		octahedron_btn.style.backgroundColor = 'lightgreen';
+		octahedron_btn.style.cursor = 'pointer';
+		octahedron_btn.style.color = 'white';
+		octahedron_btn.style.padding = '1em';
+		container.appendChild(octahedron_btn);
+
+		var hexahedron_btn = document.createElement('div');
+		hexahedron_btn.style.position = 'absolute';
+		hexahedron_btn.style.top = '190px';
+		hexahedron_btn.style.right = '20px';
+		hexahedron_btn.style.width = '8em';
+		hexahedron_btn.style.textAlign = 'center';
+		hexahedron_btn.innerHTML = 'HEXAHEDRON';
+		hexahedron_btn.style.backgroundColor = 'lightgreen';
+		hexahedron_btn.style.cursor = 'pointer';
+		hexahedron_btn.style.color = 'white';
+		hexahedron_btn.style.padding = '1em';
+		container.appendChild(hexahedron_btn);
+
+		var icosahedron_btn = document.createElement('div');
+		icosahedron_btn.style.position = 'absolute';
+		icosahedron_btn.style.top = '250px';
+		icosahedron_btn.style.right = '20px';
+		icosahedron_btn.style.width = '8em';
+		icosahedron_btn.style.textAlign = 'center';
+		icosahedron_btn.innerHTML = 'ICOSAHEDRON';
+		icosahedron_btn.style.backgroundColor = 'lightgreen';
+		icosahedron_btn.style.cursor = 'pointer';
+		icosahedron_btn.style.color = 'white';
+		icosahedron_btn.style.padding = '1em';
+		container.appendChild(icosahedron_btn);
+
+		var dodecahedron_btn = document.createElement('div');
+		dodecahedron_btn.style.position = 'absolute';
+		dodecahedron_btn.style.top = '310px';
+		dodecahedron_btn.style.right = '20px';
+		dodecahedron_btn.style.width = '8em';
+		dodecahedron_btn.style.textAlign = 'center';
+		dodecahedron_btn.innerHTML = 'DODECAHEDRON';
+		dodecahedron_btn.style.backgroundColor = 'lightgreen';
+		dodecahedron_btn.style.cursor = 'pointer';
+		dodecahedron_btn.style.color = 'white';
+		dodecahedron_btn.style.padding = '1em';
+		container.appendChild(dodecahedron_btn);
+
+
 		camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
 		camera.position.y = 150;
 		camera.position.z = 500;
 
-		scene = new THREE.Scene();
+		window.scene = new THREE.Scene();
 
-		// Cube
+		console.log(selectedPolygon);
 
-		var boxGeometry = new THREE.BoxGeometry(200, 200, 200);
-
-		for (var i = 0; i < boxGeometry.faces.length; i += 2)
-		{
-
-			var color = {
-				h: (1 / (boxGeometry.faces.length)) * i,
-				s: 0.5,
-				l: 0.5
-			};
-
-			boxGeometry.faces[i].color.setHSL(color.h, color.s, color.l);
-			boxGeometry.faces[i + 1].color.setHSL(color.h, color.s, color.l);
-
+		if(selectedPolygon == "cube"){
+			console.log('1');
+			loadCube();
 		}
+		if(selectedPolygon == "tetrahedron"){
+			console.log('2');
+			loadTetrahedron();
+		}
+		// -------------------------------------------------------------------------
 
-		var cubeMaterial = new THREE.MeshBasicMaterial(
-		{
-			vertexColors: THREE.FaceColors,
-			overdraw: 0.5
-		});
-
-		cube = new THREE.Mesh(boxGeometry, cubeMaterial);
-		cube.position.y = 200;
-		scene.add(cube);
-
-		// Plane
-
-		var planeGeometry = new THREE.PlaneGeometry(200, 200);
-		planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
-
-		var planeMaterial = new THREE.MeshBasicMaterial(
-		{
-			color: 0xe0e0e0,
-			overdraw: 0.5
-		});
-
-		plane = new THREE.Mesh(planeGeometry, planeMaterial);
-		scene.add(plane);
 
 		renderer = new THREE.CanvasRenderer();
 		renderer.setClearColor(0xf0f0f0);
@@ -111,7 +173,7 @@ Defmech.RotationWithQuaternion = (function()
 		window.addEventListener('resize', onWindowResize, false);
 
 		animate();
-	};
+	}
 
 	function onWindowResize()
 	{
@@ -246,7 +308,7 @@ Defmech.RotationWithQuaternion = (function()
 			handleRotation();
 		}
 
-		renderer.render(scene, camera);
+		renderer.render(window.scene, camera);
 	}
 
 	var handleRotation = function()
@@ -254,10 +316,20 @@ Defmech.RotationWithQuaternion = (function()
 		rotateEndPoint = projectOnTrackball(deltaX, deltaY);
 
 		var rotateQuaternion = rotateMatrix(rotateStartPoint, rotateEndPoint);
-		curQuaternion = cube.quaternion;
-		curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
-		curQuaternion.normalize();
-		cube.setRotationFromQuaternion(curQuaternion);
+
+		if(selectedPolygon === "cube"){
+			curQuaternion = window.cube.quaternion;
+			curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
+			curQuaternion.normalize();
+			window.cube.setRotationFromQuaternion(curQuaternion);
+		}
+
+		if(selectedPolygon === "tetrahedron"){
+			curQuaternion = window.tetrahedron.quaternion;
+			curQuaternion.multiplyQuaternions(rotateQuaternion, curQuaternion);
+			curQuaternion.normalize();
+			window.tetrahedron.setRotationFromQuaternion(curQuaternion);
+		}
 
 		rotateEndPoint = rotateStartPoint;
 	};
@@ -278,3 +350,98 @@ document.onreadystatechange = function()
 		Defmech.RotationWithQuaternion.init();
 	}
 };
+
+document.setPolygon = function(polygon){
+
+	if (polygon === "tetrahedron"){
+		if(selectedPolygon != "tetrahedron"){
+			selectedPolygon = "tetrahedron";
+			window.scene.remove(window.cube);
+			window.scene.remove(plane);
+			//remove other polygons;
+			loadTetrahedron();
+		}
+		else{
+			alert("Tetrahedron is already selected!")
+		}
+	}
+
+	if (polygon === "cube"){
+		if(selectedPolygon != "cube"){
+			selectedPolygon = "cube";
+			window.scene.remove(window.tetrahedron);
+			//remove other polygons;
+			loadCube();
+		}
+		else{
+			alert("Cube is already selected!");
+		}
+	}
+}
+
+function loadCube(){
+	var boxGeometry = new THREE.BoxGeometry(200, 200, 200);
+
+	for (var i = 0; i < boxGeometry.faces.length; i += 2)
+	{
+		//hue, saturation, lightning
+		var color = {
+			h: (1 / (boxGeometry.faces.length)) * i,
+			s: 0.5,
+			l: 0.5
+		};
+
+		boxGeometry.faces[i].color.setHSL(color.h, color.s, color.l);
+		boxGeometry.faces[i + 1].color.setHSL(color.h, color.s, color.l);
+
+	}
+
+	//Changing cube colors
+	var cubeMaterial = new THREE.MeshBasicMaterial(
+	{
+		vertexColors: THREE.FaceColors,
+		overdraw: 0.5
+	});
+
+	window.cube = new THREE.Mesh(boxGeometry, cubeMaterial);
+	window.cube.position.y = 200;
+	window.scene.add(window.cube);
+
+	// Plane
+	var planeGeometry = new THREE.PlaneGeometry(200, 200);
+	planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+	var planeMaterial = new THREE.MeshBasicMaterial(
+	{
+		color: 0xe0e0e0,
+		overdraw: 0.5
+	});
+
+	plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	window.scene.add(plane);
+
+}
+
+
+function loadTetrahedron(){
+	//Changing tetrahedron colors
+	var tetrahedronMaterial = new THREE.MeshBasicMaterial(
+	{
+		vertexColors: THREE.FaceColors,
+		overdraw: 0.5
+	});
+
+	THREE.TetrahedronGeometry = function ( radius, detail ) {
+
+	var vertices = [ 1,  1,  1,   - 1, - 1,  1,   - 1,  1, - 1,    1, - 1, - 1];
+	var indices = [ 2,  1,  0,    0,  3,  2,    1,  3,  0,    2,  3,  1];
+
+	THREE.PolyhedronGeometry.call( this, vertices, indices, radius, detail );
+};
+	console.log('ok');
+
+	THREE.TetrahedronGeometry.prototype = Object.create( THREE.Geometry.prototype );
+	window.tetrahedron = new THREE.Mesh(new THREE.TetrahedronGeometry(50), tetrahedronMaterial);
+	console.log(window.tetrahedron);
+	window.scene.add(window.tetrahedron);
+}
